3da3e9eba3f7c91ca13a27815ba897f2e59fe285
rohitdigraskar1 <126749248+rohitdigraskar1@users.noreply.github.com>
2023-04-03 18:16:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit as st
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+rand=np.random.normal(1, 2, size=20)fig, ax = plt.subplots()
+ax.hist(rand, bins=15)st.pyplot(fig)
 st.write("""Hello world  my 1st streamlit app""")
+
